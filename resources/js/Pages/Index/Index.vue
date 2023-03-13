16499489fb4eec11b14bf1f355b0976a2595ceca
Nirmal Adhikari <nirmal@buildchange.org>
2023-03-13 11:06:10 +0545

    persistant layout sample

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Index</div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import MainLayout from '../../Layouts/MainLayout.vue';
+defineProps({
+    message: String
+})
+</script>
+
+<script>
+export default{
+    layout: MainLayout
+}</script>
+
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import MainLayout from '../../Layouts/MainLayout.vue';
+</script>
+<script>
+export default{
+    layout: MainLayout
+}</script>
+
+
